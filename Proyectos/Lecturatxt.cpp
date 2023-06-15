b3faff051e611ab91a6fd9b4f02681c8eddafeaa
Agustin Silva Rojas <asilva@ofinet.cl>
2023-06-15 16:38:51 -0400

    Subida avance lecturatxt, falta validar letras con tilde y a enie

--- Proyectos/Lecturatxt.cpp
+++ Proyectos/Lecturatxt.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 int main() {
 int nBlancos=0, nDigitos=0, nLetras=0, nNulineas=0, nOtros=0, c = 0;
 FILE *fichero;
@@ -9,12 +11,34 @@ if(!(fichero = fopen("IntProg_U4.txt", "rt"))){
 	
 }
 
-fgets(letras,100,fichero);
-printf("%s",letras);
-//while (c=fgetc(fichero) == EOL)
-//{
-//aqui va tu analisis y conteo
-//}
-//printf("%10s%10s%10s%10s%10s%10s\n\n","blancos", "digitos", "letras", "lineas", "otros", "total");
-//printf("%10d%10d%10d%10d%10d%10d\n\n", nBlancos, nDigitos, nLetras, nNulineas, nOtros,nBlancos+nDigitos+nLetras+nNulineas+nOtros);
+
+while(fgets(letras,100,fichero)){
+	for(int i = 0; i< strlen(letras);i++){
+		printf("%c\n",letras[i]);	
+		switch(letras[i]){
+			case '\n':
+				nNulineas ++;
+			break;
+			case ' ':
+				nBlancos ++;
+			break;
+			default:
+			 if(isdigit(letras[i])){
+				nDigitos ++;
+			 }else if(isalpha(letras[i])){
+			 	nLetras ++;
+			 }else{
+			 nOtros ++;
+			}
+		}
+		
+	}
+	//printf("%s",letras);
+	
+}
+
+
+
+printf("%10s%10s%10s%10s%10s%10s\n\n","blancos", "digitos", "letras", "lineas", "otros", "total");
+printf("%10d%10d%10d%10d%10d%10d\n\n", nBlancos, nDigitos, nLetras, nNulineas, nOtros,nBlancos+nDigitos+nLetras+nNulineas+nOtros);
 }
