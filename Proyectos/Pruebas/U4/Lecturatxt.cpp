53c37b02165be132b8c2d92e087e233d62bc9f6c
Agustin Silva Rojas <asilva@ofinet.cl>
2023-06-15 18:55:35 -0400

    Mejorando la ultima linea vacia, no la considera

--- Proyectos/Pruebas/U4/Lecturatxt.cpp
+++ Proyectos/Pruebas/U4/Lecturatxt.cpp
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <locale.h>
 int main() {
 int nBlancos=0, nDigitos=0, nLetras=0, nNulineas=0, nOtros=0, c = 0;
 FILE *fichero;
 char letras[100];
+char Vfichero[100];
+setlocale(LC_CTYPE, "Spanish");
 
 if(!(fichero = fopen("IntProg_U4.txt", "rt"))){
 	
@@ -13,27 +16,34 @@ if(!(fichero = fopen("IntProg_U4.txt", "rt"))){
 
 
 while(fgets(letras,100,fichero)){
+	
+	printf("Frase: %s\n",letras);
 	for(int i = 0; i< strlen(letras);i++){
-		printf("%c\n",letras[i]);	
 		switch(letras[i]){
 			case '\n':
 				nNulineas ++;
+				printf("Caracter: Salto \n");	
 			break;
 			case ' ':
 				nBlancos ++;
+				printf("Caracter: Blanco\n");	
 			break;
+			case '\r':
+				printf("Caracter: UltimaLinea");
 			default:
 			 if(isdigit(letras[i])){
 				nDigitos ++;
+				printf("Caracter: %c\n",letras[i]);	
 			 }else if(isalpha(letras[i])){
 			 	nLetras ++;
+			 	printf("Caracter: %c\n",letras[i]);	
 			 }else{
-			 nOtros ++;
+				 nOtros ++;
+				 printf("Caracter: %c\n",letras[i]);	
 			}
 		}
 		
 	}
-	//printf("%s",letras);
 	
 }
 
