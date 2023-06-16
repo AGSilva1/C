7670dd1844e160a844aeb3a29da559cf3e0fcd2f
Agustin Silva <agstnsilva@outlook.com>
2023-06-15 21:42:20 -0400

    Termino, falta validar pruebas 2 y 3

--- Proyectos/Pruebas/U4/Lecturatxt.cpp
+++ Proyectos/Pruebas/U4/Lecturatxt.cpp
@@ -4,39 +4,45 @@
 #include <ctype.h>
 #include <locale.h>
 int main() {
-int nBlancos=0, nDigitos=0, nLetras=0, nNulineas=0, nOtros=0, c = 0;
+//se inicializan los contadores de los ipos de caracteres
+int nBlancos=0, nDigitos=0, nLetras=0, nNulineas=0, nOtros=0;
+//Se inicializa la variable de tipo archivo para abrir y lerr el archivo
 FILE *fichero;
+//Se inicializa varibla de largo 100 para recorrer los caracteres del txt
 char letras[100];
-char Vfichero[100];
-setlocale(LC_CTYPE, "Spanish");
 
-if(!(fichero = fopen("IntProg_U4.txt", "rt"))){
+//Se abre el archivo enmodo lectura
+fichero = fopen("IntProg_U4.txt", "r");
 	
-}
-
-
+//Mientras exista linea ejecuta expresion interna
 while(fgets(letras,100,fichero)){
-	
-	printf("Frase: %s\n",letras);
+	//por cada linea se suma al contador de lineas
+	nNulineas ++;
+	//printf("Frase: %s\n",letras);
+	//Recorre la linea caracter por caracter
 	for(int i = 0; i< strlen(letras);i++){
+		//segun el tipo de caracter sumar
 		switch(letras[i]){
+			//Si es salto de linea, no hacer nada ya que lo sumamos al inicio del while y no queremos que lo considere el default
 			case '\n':
-				nNulineas ++;
-				printf("Caracter: Salto \n");	
-			break;
+				
+				break;
+			//Se es espacio en blanco sumar en su contador
 			case ' ':
 				nBlancos ++;
-				printf("Caracter: Blanco\n");	
-			break;
-			case '\r':
-				printf("Caracter: UltimaLinea");
+				//printf("Caracter: Blanco\n");	
+				break;
+			//Encaso de no ser ninguno de los otros dos casos
 			default:
+			//Valida si el caracter es un numero, suma a su contador
 			 if(isdigit(letras[i])){
 				nDigitos ++;
-				printf("Caracter: %c\n",letras[i]);	
+				//printf("Caracter: %c\n",letras[i]);	
+			//En caso contrario valida si es una letra  suma a su contador
 			 }else if(isalpha(letras[i])){
 			 	nLetras ++;
-			 	printf("Caracter: %c\n",letras[i]);	
+			 	//printf("Caracter: %c\n",letras[i]);	
+			//En caso de no ser letra o numero se suma a contador de otros caracteres
 			 }else{
 				 nOtros ++;
 				 printf("Caracter: %c\n",letras[i]);	
@@ -48,7 +54,9 @@ while(fgets(letras,100,fichero)){
 }
 
 
-
+//Imprimer los titulos de cada contador y el total de todos
 printf("%10s%10s%10s%10s%10s%10s\n\n","blancos", "digitos", "letras", "lineas", "otros", "total");
+// imprime las cantidades segun titulo y la suma de todos
 printf("%10d%10d%10d%10d%10d%10d\n\n", nBlancos, nDigitos, nLetras, nNulineas, nOtros,nBlancos+nDigitos+nLetras+nNulineas+nOtros);
+
 }
