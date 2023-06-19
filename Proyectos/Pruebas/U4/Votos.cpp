929c9a564cbdde35d9e78f3be5fd18119f0df895
Agustin Silva <agstnsilva@outlook.com>
2023-06-18 21:50:56 -0400

    Termino prueba votos

--- Proyectos/Pruebas/U4/Votos.cpp
+++ Proyectos/Pruebas/U4/Votos.cpp
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 struct datos
 {
 char nombre[40];
@@ -15,35 +17,57 @@ struct datos pers;
 int main()
 {
 	int i,posmay,maynmed=0,cand;
-	char nomIngresa; char* coaliIngresa;
 	int poscIngresa; int votIngresa;
 	do{
-		printf("Ingrese numero de candidatos");scanf("%d",&cand);
-	} while (cand<=1);
+		printf("Ingrese numero de candidatos:\n");
+		scanf("%d",&cand);
+	}while (cand<2);
+	
 	struct candidato municipal[cand];
 	
+	
+	for(i=0;i<cand;i++){
+		printf("Ingrese datos del candidato %d: \n",i+1);
+		do{			 
+			printf("Nombre: ");
+			scanf("%s",&municipal[i].pers.nombre);
+			//printf("Candidato N: %s\n", municipal[i].pers.nombre);
+		}while(municipal[i].pers.nombre=="");
+		do{
+			printf("Coalision: ");
+			scanf("%s",&municipal[i].pers.coalision);
+			//printf("Candidato C: %s\n", municipal[i].pers.coalision);	
+		}while(municipal[i].pers.coalision=="");
+		do{
+			printf("Posicion: ");
+			scanf("%d",&municipal[i].posicion);
+			//printf("Candidato P: %d: \n", municipal[i].posicion);
+			
+		}while(municipal[i].posicion<0);	 
+			 
+		do{
+			printf("Votos: ");
+			scanf("%d",&municipal[i].votos);
+			//printf("Candidato V: %d\n", municipal[i].votos);
+		}while(municipal[i].votos<0);
+	
+	}
 	for(i=0;i<cand;i++)
 	{
-		 printf("Ingrese datos del candidato %d: \n",cand);
-		 printf("Nombre: ");
-		 scanf("%c",municipal[i].pers.nombre);
-		 printf("Coalision: ");
-		 scanf("%c",municipal[i].pers.coalision);
-		 printf("Posicion: ");
-		 scanf("%d",municipal[i].posicion);
-		 printf("Votos: ");
-		 scanf("%d",municipal[i].votos);
-	}
-	
-	/*for(i=0;i<cand;i++)
-	{
-		 // aquí la obtencion del ganador
+		/*printf("\n\t****DATOS DEL CANDIDATO CON MAYOR NUMERO DE VOTOS****\n");
+		printf("NOMBRE: %s\n",municipal[i].pers.nombre);
+		printf("COALICION: %s\n",municipal[i].pers.coalision);
+		printf("POSICION: %d\n",municipal[i].posicion);
+		printf("NUMERO DE VOTOS: %d\n",municipal[i].votos);*/
+		if(municipal[i].votos>maynmed){
+			maynmed = municipal[i].votos;
+			posmay = i;
+		}
 	}
 	printf("\n\t****DATOS DEL CANDIDATO CON MAYOR NUMERO DE VOTOS****\n");
 	printf("NOMBRE: %s\n",municipal[posmay].pers.nombre);
 	printf("COALICION: %s\n",municipal[posmay].pers.coalision);
 	printf("POSICION: %d\n",municipal[posmay].posicion);
 	printf("NUMERO DE VOTOS: %d\n",municipal[posmay].votos);
-	*/
 	return 0;
 }
